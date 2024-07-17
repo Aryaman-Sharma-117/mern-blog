473172d4589073a1578eaf1a1328bdef999174bd
Aryaman Sharma <aryamansharma.india@gmail.com>
2024-07-18 04:35:11 +0530

    Routes

--- ...client/src/App.jsx
+++ ...client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {Routes,Route,BrowserRouter} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import DashBoard from './pages/DashBoard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 export default function App() {
   return (
-    <div>Apple</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/dashboard" element={<DashBoard/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 

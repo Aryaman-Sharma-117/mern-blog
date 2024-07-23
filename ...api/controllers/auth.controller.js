eff6f8bee4be746bf48c7edc33adf07ec84b1ab9
Aryaman Sharma <aryamansharma.india@gmail.com>
2024-07-23 13:19:21 +0530

    header and dark mode

--- ...api/controllers/auth.controller.js
+++ ...api/controllers/auth.controller.js
@@ -53,9 +53,10 @@ export const google = async (req,res,next)=>{
     try{
         const user = await User.findOne({email})
         if(user){
-            const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
-            const {password:pass,...rest}=validUser._doc;
+            const token = jwt.sign({id:user._id},process.env.JWT_SECRET);
+            const {password:pass,...rest}=user._doc;
             res.status(200).cookie('access_token',token,{httpOnly:true}).json(rest);
+            console.log('success')
         } else{
             const generatedPassword = Math.random().toString(36).slice(-8)
             const hashedPassword = bcryptjs.hashSync(generatedPassword,10)
@@ -66,8 +67,8 @@ export const google = async (req,res,next)=>{
                 profilePicture:googlePhotoURL,
             })
             await newUser.save()
-            const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
-            const {password:pass,...rest}=validUser._doc;
+            const token = jwt.sign({id:newUser._id},process.env.JWT_SECRET);
+            const {password:pass,...rest}=newUser._doc;
             res.status(200).cookie('access_token',token,{httpOnly:true}).json(rest);
         }
     } catch(error){
--- ...client/src/components/Header.jsx
+++ ...client/src/components/Header.jsx
@@ -1,11 +1,16 @@
-import {Button, Navbar, TextInput} from 'flowbite-react'
+import {Avatar, Button, Dropdown, Navbar, TextInput} from 'flowbite-react'
 import { Link,useLocation } from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai'
-import {FaMoon} from 'react-icons/fa'
+import {FaMoon,FaSun} from 'react-icons/fa'
 import { useState } from 'react'
+import {useSelector,useDispatch} from 'react-redux'
+import { toggleTheme } from '../redux/theme/themeSlice'
 
 export default function Header() {
     const path = useLocation().pathname;
+    const {currentUser} = useSelector(state=>state.user)
+    const dispatch = useDispatch()
+    const {theme} = useSelector(state=>state.theme)
     const [isNavbarCollapsed, setIsNavbarCollapsed] = useState(true)
     const handleToggle = () => {
         setIsNavbarCollapsed(!isNavbarCollapsed);
@@ -30,14 +35,32 @@ export default function Header() {
                 <AiOutlineSearch/>
             </Button>
             <div className='flex gap-2 md:order-2'>
-                <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-                    <FaMoon/>
+                <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={()=>dispatch(toggleTheme())}>
+                    {theme === 'dark' ? <FaSun/> : <FaMoon/>}
                 </Button>
-                <Link to='/sign-in'>
+                {currentUser ? (
+                    <Dropdown arrowIcon={false} inline label={<Avatar alt='user' img={currentUser.profilePicture} rounded/>}>
+                        <Dropdown.Header>
+                           <span className='block text-sm'>@{currentUser.username}</span>
+                           <span className='block text-sm truncate font-medium'>{currentUser.email}</span>
+                        </Dropdown.Header>
+                        <Link to={'/dashboard?tab=profile'}>
+                            <Dropdown.Item >Profile</Dropdown.Item>
+                        </Link>
+                        <Dropdown.Divider/>
+                        <Dropdown.Item>Sign Out</Dropdown.Item>
+                    </Dropdown>
+                ) :
+                    (
+                    <Link to='/sign-in'>
                     <Button gradientDuoTone='purpleToBlue' outline>
                         Sign-in
                     </Button>
-                </Link>
+                    </Link>
+                    )
+                }
+
+                
                 <Navbar.Toggle onClick={handleToggle}/>
             </div>
             <Navbar.Collapse className={`flex lg:items-center lg:w-auto ${isNavbarCollapsed ? 'hidden' : 'block'}`}>
